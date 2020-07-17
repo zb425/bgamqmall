de144c04b1926ec4abd1240ed5b73b5a0ae12b49
zb425 <1047441153@qq.com>
2020-07-17 16:06:42 +0800

    创建其它服务模块

---
+++ gulimall-sms/src/test/java/com/bgamq/gulimall/sms/GulimallSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgamq.gulimall.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ums/src/test/java/com/bgamq/gulimall/ums/GulimallUmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgamq.gulimall.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallUmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
